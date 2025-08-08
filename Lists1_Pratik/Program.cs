17d537561ede35d2274c57076b6ed5e7ba4c7f74
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-08-09 01:03:36 +0300

    Add project files.

---
+++ Lists1_Pratik/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> davetliler = new List<string>
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+        Console.WriteLine("** Davetliler **");
+
+        // foreach ile yazdırma
+        int sira = 1;
+        foreach (var kisi in davetliler)
+        {
+            Console.WriteLine($"{sira} - {kisi}");
+            sira++;
+        }
+    }
+}
